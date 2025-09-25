e778ee24b6580d720a2f1da1a71c87abd564fd98
MizaelBarreto <mizaelmbarreto@gmail.com>
2025-09-25 17:34:27 -0300

    corrigindo dotenv

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // server.js
-import "dotenv/config"; // <- carrega variáveis imediatamente antes de qualquer uso
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { pool } from "./db.js";
@@ -7,7 +7,7 @@ import { createObjectCsvWriter } from "csv-writer";
 import fs from "fs";
 import path from "path";
 
-dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "2mb" }));
